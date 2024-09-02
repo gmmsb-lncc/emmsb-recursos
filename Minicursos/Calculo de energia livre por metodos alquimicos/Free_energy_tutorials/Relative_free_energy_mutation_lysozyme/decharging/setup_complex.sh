dbb21bb4ac3edfbfe6c09424e54d076b971288c4
Leon Sulfierry <sulfierry@gmail.com>
2024-09-02 17:14:11 -0300

    Add files via upload

---
+++ Minicursos/Calculo de energia livre por metodos alquimicos/Free_energy_tutorials/Relative_free_energy_mutation_lysozyme/decharging/setup_complex.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#
+# Setup for the free energy simulations: creates and links to the input file as
+# necessary.  Two alternative for the de- and recharging step can be used.
+#
+
+
+. ./windows
+
+# partial removal/addition of charges: softcore atoms only
+decharge_crg=":163"
+
+decharge=" ifsc = 0, crgmask = '$decharge_crg',"
+
+basedir=../setup
+top=$(pwd)
+setup_dir=$(cd "$basedir"; pwd)
+
+for system in complex; do
+  if [ \! -d $system ]; then
+    mkdir $system
+  fi
+
+  cd $system
+
+  for step in decharge; do
+    if [ \! -d $step ]; then
+      mkdir $step
+    fi
+
+    cd $step
+
+    for w in $windows; do
+      if [ \! -d $w ]; then
+        mkdir $w
+      fi
+
+      FE=$(eval "echo \${$step}")
+      sed -e "s/%L%/$w/" -e "s/%FE%/$FE/" $top/min_complex.tmpl > $w/min.in
+      sed -e "s/%L%/$w/" -e "s/%FE%/$FE/" $top/heat_complex.tmpl > $w/heat.in
+      sed -e "s/%L%/$w/" -e "s/%FE%/$FE/" $top/eqnpt_complex.tmpl > $w/eqnpt.in
+      sed -e "s/%L%/$w/" -e "s/%FE%/$FE/" $top/prod_complex.tmpl > $w/ti.in
+
+      (
+        cd $w
+        ln -sf $setup_dir/merged_lyz_bnz_dech.parm7 ti.parm7
+        ln -sf $setup_dir/merged_lyz_bnz_dech.rst7  ti.rst7
+      )
+    done
+
+    cd ..
+  done
+
+  cd $top
+done
+
