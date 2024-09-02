5f8d60bbddb80ae82b60a05d9451ab1c06d11919
Leon Sulfierry <sulfierry@gmail.com>
2024-09-02 17:15:41 -0300

    Add files via upload

---
+++ Minicursos/Calculo de energia livre por metodos alquimicos/Free_energy_tutorials/Relative_free_energy_mutation_lysozyme/recharging/prod2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#windows=`echo "0.00 0.05 0.10 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.47 0.50 0.53 0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.92 0.94 0.95 0.97 0.99 1.00"`
+windows=`echo "0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.92 0.94 0.95 0.97 0.99 1.00"`
+for i in $windows
+do
+cd $i
+export AMBERHOME=~/sortware/amber18
+source ~/software/amber18/amber.sh
+
+export CUDA_VISIBLE_DEVICES=1
+
+pmemd.cuda -i heat.in -c ti.rst7 -ref ti.rst7 -p ti.parm7 -O -o heat.out -inf heat.info -e heat.en -r heat.rst7 -x heat.nc -l heat.log
+pmemd.cuda -i eqnpt.in -c heat.rst7 -p ti.parm7 -O -o eqnpt.out -inf eqnpt.info -e eqnpt.en -r eqnpt.rst7 -x eqnpt.nc -l eqnpt.log
+pmemd.cuda -i ti.in -c eqnpt.rst7 -p ti.parm7 -O -o ti001.out -inf ti001.info -e ti001.en -r ti001.rst7 -x ti001.nc -l ti001.log
+cd ../
+done
---
+++ Minicursos/Calculo de energia livre por metodos alquimicos/Free_energy_tutorials/Relative_free_energy_mutation_lysozyme/recharging/setup_complex.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#
+# Setup for the free energy simulations: creates and links to the input file as
+# necessary.  Two alternative for the de- and recharging step can be used.
+#
+
+
+. ./windows
+
+# partial removal/addition of charges: softcore atoms only
+recharge_crg=":99"
+
+recharge=" ifsc = 0, crgmask = '$recharge_crg',"
+
+basedir=../setup
+top=$(pwd)
+setup_dir=$(cd "$basedir"; pwd)
+
+for system in complex; do
+  if [ \! -d $system ]; then
+    mkdir $system
+  fi
+
+  cd $system
+
+  for step in recharge; do
+    if [ \! -d $step ]; then
+      mkdir $step
+    fi
+
+    cd $step
+
+    for w in $windows; do
+      if [ \! -d $w ]; then
+        mkdir $w
+      fi
+
+      FE=$(eval "echo \${$step}")
+      sed -e "s/%L%/$w/" -e "s/%FE%/$FE/" $top/min_complex.tmpl > $w/min.in
+      sed -e "s/%L%/$w/" -e "s/%FE%/$FE/" $top/heat_complex.tmpl > $w/heat.in
+      sed -e "s/%L%/$w/" -e "s/%FE%/$FE/" $top/eqnpt_complex.tmpl > $w/eqnpt.in
+      sed -e "s/%L%/$w/" -e "s/%FE%/$FE/" $top/prod_complex.tmpl > $w/ti.in
+
+      (
+        cd $w
+        ln -sf $setup_dir/merged_mlz_bnz_rech.parm7 ti.parm7
+        ln -sf $setup_dir/merged_mlz_bnz_rech.rst7  ti.rst7
+      )
+    done
+
+    cd ..
+  done
+
+  cd $top
+done
+
